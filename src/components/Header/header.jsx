b589fcf73e40fe1a8e16a3c0dfdde6b1eadf1a12
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-09-29 17:14:22 +0300

    Took out the PopUser

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,3 +1,5 @@
+import PopUser from "../PopUser/PopUser"
+
 function Header () {
     return (
         <>
@@ -21,24 +23,7 @@ function Header () {
                     <a href="#user-set-target" className="header__user _hover02">
                     Ivan Ivanov
                     </a>
-                    <div
-                    className="header__pop-user-set pop-user-set"
-                    id="user-set-target"
-                    >
-                    <p className="pop-user-set__name">Ivan Ivanov</p>
-                    <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                    <div className="pop-user-set__theme">
-                        <p>Темная тема</p>
-                        <input
-                        type="checkbox"
-                        className="checkbox"
-                        name="checkbox"
-                        ></input>
-                    </div>
-                    <button type="button" className="_hover03">
-                        <a href="#popExit">Выйти</a>
-                    </button>
-                    </div>
+                    <PopUser></PopUser>
                 </nav>
                 </div>
             </div>
