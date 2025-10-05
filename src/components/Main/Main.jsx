d19c79cf39bcbdd0572ee3a0db033fdc1be04671
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-10-05 23:17:57 +0300

    Edited Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,44 @@
 import { Column } from "../Column/Column";
 import { CardList } from "../data";
 
+const COLUMN_TITLES = [
+  "Без статуса",
+  "Нужно сделать",
+  "В работе",
+  "Тестирование",
+  "Готово",
+];
+
 export function Main() {
+  const tasksByStatus = () => {
+    const indexed = COLUMN_TITLES.reduce((acc, s) => {
+      acc[s] = [];
+
+      return acc;
+    }, {});
+
+    for (const t of CardList) {
+      const key = COLUMN_TITLES.includes(t.status) ? t.status : "Без статуса";
+
+      indexed[key].push(t);
+    }
+
+    return indexed;
+  };
+
   return (
     <>
       <main className="main">
         <div className="container">
           <div className="main__block">
             <div className="main__content">
-              <Column cards={CardList.filter((card) => card.status === "Без статуса")} status={"Без статуса"}/>
-              <Column cards={CardList.filter((card) => card.status === "Нужно сделать")} status={"Нужно сделать"}/>
-              <Column cards={CardList.filter((card) => card.status === "В работе")} status={"В работе"}/>
-              <Column cards={CardList.filter((card) => card.status === "Тестирование")} status={"Тестирование"}/>
-              <Column cards={CardList.filter((card) => card.status === "Готово")} status={"Готово"}/>
+              {COLUMN_TITLES.map((title) => (
+                <Column
+                  key={title}
+                  cards={tasksByStatus()[title]}
+                  status={title}
+                />
+              ))}
             </div>
           </div>
         </div>
